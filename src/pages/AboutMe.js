266645e69839513a0c20f43fba1c7d3dcb0a1526
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2023-08-03 00:02:44 +0100

    finished adding projects also the portfolio

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,6 +1,4 @@
 import { motion } from "framer-motion";
-import Skill from "../modules/Skill";
-
 import classe from "./layout/AboutMe.module.css";
 import ProfileImage from "../assets/vitor.jpeg";
 
