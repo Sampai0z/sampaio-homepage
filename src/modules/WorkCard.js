79c152a0207cb6cb881268a3ccf4cbcefa7878f8
Vitor Sampaio <vitor-sampaio@outlook.com.br>
2024-12-18 11:39:41 +0000

    adding image to mokuteki project and refactoring

--- src/modules/WorkCard.js
+++ src/modules/WorkCard.js
@@ -15,7 +15,7 @@ const WorkCard = (props) => {
     <div className="col-md-6 pb-5">
       <div className="col-md-12">
         <div className="quadro">
-          <img src={props.src} alt="teste" width={"100%"} />
+          <img src={props.src} alt="teste" width={"90%"} />
         </div>
       </div>
       <div className="col-md-12">
